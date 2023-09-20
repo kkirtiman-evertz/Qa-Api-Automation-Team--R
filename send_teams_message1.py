1d3463c9044252edd6f09e5ae63ac82b99ea9369
Kumar Kirtiman <122998286+kkirtiman-evertz@users.noreply.github.com>
2023-09-21 03:30:29 +0530

    Update send_teams_message1.py

--- send_teams_message1.py
+++ send_teams_message1.py
@@ -35,26 +35,26 @@
             "entities": [
               {
                 "type": "mention",
-                "text": "<at>USER NAME</at>",
+                "text": "<at>Kumar Kirtiman</at>",
                 "mentioned": {
-                  "id": "8:orgid:USER AAD ID",
-                  "name": "USER NAME"
+                  "id": "kkirtiman@evertz.com",
+                  "name": "Kumar Kirtiman"
                 }
               },
               {
                 "type": "mention",
-                "text": "<at>TAG NAME</at>",
+                "text": "<at>CS Release QA</at>",
                 "mentioned": {
-                  "id": "TAG ID",
-                  "name": "TAG NAME"
+                  "id": "CS Release QA",
+                  "name": "CS Release QA"
                 }
               },
               {
                 "type": "mention",
-                "text": "<at>CHANNEL NAME</at>",
+                "text": "<at>NotificationTest</at>",
                 "mentioned": {
-                  "id": "CHANNEL ID",
-                  "displayName": "CHANNEL NAME",
+                  "id": "19%3a7aca7755587a42a995e838b64712ca7f%40thread.tacv2",
+                  "displayName": "NotificationTest",
                   "conversationIdentityType": "channel",
                   "conversationIdentityType@odata.type": "#Microsoft.Teams.GraphSvc.conversationIdentityType"
                 }
@@ -65,3 +65,12 @@
       }
     ]
   }
+teams_webhook_url = "https://evertz1.webhook.office.com/webhookb2/33be58bf-bed8-4287-bd32-7b739fd3a2f6@e7ca1d1b-0b74-449f-8cc2-a9865bfc0a5f/IncomingWebhook/3fd543a567cb4d55ad46646ef1a798e2/bb4014fc-682b-4603-bb76-e94aff3c8d10"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(teams_webhook_url, json=message, headers=headers)
+    if response.status_code == 200:
+        print("Reminder sent successfully.")
+    else:
+        print("Failed to send reminder.")
