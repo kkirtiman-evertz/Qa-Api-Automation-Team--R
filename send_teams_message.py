8f323efc372cf54c9f247c6340fc4fb066002657
Kumar Kirtiman <122998286+kkirtiman-evertz@users.noreply.github.com>
2023-09-20 16:50:20 +0530

    Update send_teams_message.py

--- send_teams_message.py
+++ send_teams_message.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace with your Microsoft Teams webhook URL
-webhook_url = "https://evertz1.webhook.office.com/webhookb2/33be58bf-bed8-4287-bd32-7b739fd3a2f6@e7ca1d1b-0b74-449f-8cc2-a9865bfc0a5f/IncomingWebhook/3fd543a567cb4d55ad46646ef1a798e2/bb4014fc-682b-4603-bb76-e94aff3c8d10%22"
+webhook_url = "https://evertz1.webhook.office.com/webhookb2/33be58bf-bed8-4287-bd32-7b739fd3a2f6@e7ca1d1b-0b74-449f-8cc2-a9865bfc0a5f/IncomingWebhook/3fd543a567cb4d55ad46646ef1a798e2/bb4014fc-682b-4603-bb76-e94aff3c8d10"
 
 # Mention user or team
 mention = {
