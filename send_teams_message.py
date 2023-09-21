5ca1d0f04788f713c99933eeb9ab23da3dbd87c2
Kumar Kirtiman <122998286+kkirtiman-evertz@users.noreply.github.com>
2023-09-21 13:30:24 +0530

    Update send_teams_message.py

--- send_teams_message.py
+++ send_teams_message.py
@@ -1,13 +1,17 @@
 import requests
 
  
+
+
 people_queue = [
-    {"name": "Shankhajit Sen","mail":"ssen@evertz.com"},
+    {"name": "Shankhajit Sen", "mailId": "kkirtiman@evertz.com"},
 ]
 
  
+
+
 def send_reminder(person):
-    mention = person['name']  # Mention by name
+    mention = f"<at>{person['name']}</at>"
     message = {
         "type": "message",
         "attachments": [
@@ -25,7 +29,7 @@ def send_reminder(person):
                         {
                             "type": "TextBlock",
                             "wrap": True,
-                            "text": f"Hi {mention} team, This is a test to check whether the tags are working fine or not. Please ignore it. Thank you!"
+                            "text": f"Hi {mention} team, This a test to check whether the tags are working fine or not.Please ignore it Thank you!"
                         }
                     ],
                     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
@@ -36,6 +40,10 @@ def send_reminder(person):
                             {
                                 "type": "mention",
                                 "text": mention,
+                                "mentioned": {
+                                    "id": person['mailId'],
+                                    "name": person['name']
+                                }
                             }
                         ]
                     }
@@ -43,7 +51,6 @@ def send_reminder(person):
             }
         ]
     }
-
     teams_webhook_url = "https://evertz1.webhook.office.com/webhookb2/33be58bf-bed8-4287-bd32-7b739fd3a2f6@e7ca1d1b-0b74-449f-8cc2-a9865bfc0a5f/IncomingWebhook/3fd543a567cb4d55ad46646ef1a798e2/bb4014fc-682b-4603-bb76-e94aff3c8d10"
     headers = {
         "Content-Type": "application/json"
@@ -54,8 +61,8 @@ def send_reminder(person):
     else:
         print("Failed to send reminder.")
 
+ 
+
 # Send reminders to each person in the queue
 for person in people_queue:
     send_reminder(person)
-
-
