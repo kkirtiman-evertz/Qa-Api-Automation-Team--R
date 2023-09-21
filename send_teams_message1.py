6f481dfcfd9b580943762d945993e1d0693b21ac
Kumar Kirtiman <122998286+kkirtiman-evertz@users.noreply.github.com>
2023-09-21 14:22:50 +0530

    Update send_teams_message1.py

--- send_teams_message1.py
+++ send_teams_message1.py
@@ -31,11 +31,11 @@ message = {
             }]
     }
 teams_webhook_url = "https://evertz1.webhook.office.com/webhookb2/33be58bf-bed8-4287-bd32-7b739fd3a2f6@e7ca1d1b-0b74-449f-8cc2-a9865bfc0a5f/IncomingWebhook/3fd543a567cb4d55ad46646ef1a798e2/bb4014fc-682b-4603-bb76-e94aff3c8d10"
-    headers = {
+headers = {
         "Content-Type": "application/json"
     }
-    response = requests.post(teams_webhook_url, json=message, headers=headers)
-    if response.status_code == 200:
-        print("Reminder sent successfully.")
-    else:
-        print("Failed to send reminder.")
+response = requests.post(teams_webhook_url, json=message, headers=headers)
+if response.status_code == 200:
+   print("Reminder sent successfully.")
+else:
+   print("Failed to send reminder.")
