a75e221234f0873416204951a6ed1fb5e7532189
Kumar Kirtiman <122998286+kkirtiman-evertz@users.noreply.github.com>
2023-09-21 18:20:20 +0530

    Update send_teams_message.py

--- send_teams_message.py
+++ send_teams_message.py
@@ -1,15 +1,17 @@
 import requests
 
+ 
+
+
 people_queue = [
-    {"name": "Kumar", "mailId": "kkirtiman@evertz.com"},
-   
+    {"name": "Shankhajit Sen", "mailId": "kkirtiman@evertz.com"},
 ]
 
-def create_mention(person):
-    mention = f"<at>{person['name']}</at>"
-    return mention
+ 
 
-def send_reminder(mentions):
+
+def send_reminder(person):
+    mention = f"<at>{person['name']}</at>"
     message = {
         "type": "message",
         "attachments": [
@@ -27,7 +29,7 @@ def send_reminder(mentions):
                         {
                             "type": "TextBlock",
                             "wrap": True,
-                            "text": f"Hi {', '.join(mentions)} team, This is a test to check whether the tags are working fine or not. Please ignore it. Thank you!"
+                            "text": f"Hi {mention} team, This a test to check whether the tags are working fine or not.Please ignore it Thank you!"
                         }
                     ],
                     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
@@ -37,10 +39,11 @@ def send_reminder(mentions):
                         "entities": [
                             {
                                 "type": "mention",
-                                "text": ', '.join(mentions),
-                                "mentioned": [
-                                    {"id": person['mailId'], "name": person['name']} for person in people_queue
-                                ]
+                                "text": mention,
+                                "mentioned": {
+                                    "id": person['mailId'],
+                                    "name": person['name']
+                                }
                             }
                         ]
                     }
@@ -48,19 +51,18 @@ def send_reminder(mentions):
             }
         ]
     }
-    teams_webhook_url = "https://evertz1.webhook.office.com/webhookb2/33be58bf-bed8-4287-bd32-7b739fd3a2f6@e7ca1d1b-0b74-449f-8cc2-a9865bfc0a5f/IncomingWebhook/3fd543a567cb4d55ad46646ef1a798e2/bb4014fc-682b-4603-bb76-e94aff3c8d10"  # Replace with your actual webhook URL
+    teams_webhook_url = "https://evertz1.webhook.office.com/webhookb2/33be58bf-bed8-4287-bd32-7b739fd3a2f6@e7ca1d1b-0b74-449f-8cc2-a9865bfc0a5f/IncomingWebhook/3fd543a567cb4d55ad46646ef1a798e2/bb4014fc-682b-4603-bb76-e94aff3c8d10"
     headers = {
         "Content-Type": "application/json"
     }
-    try:
-        response = requests.post(teams_webhook_url, json=message, headers=headers)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
+    response = requests.post(teams_webhook_url, json=message, headers=headers)
+    if response.status_code == 200:
         print("Reminder sent successfully.")
-    except Exception as e:
-        print(f"Failed to send reminder: {str(e)}")
+    else:
+        print("Failed to send reminder.")
 
-# Create mentions for all people in the queue
-mentions = [create_mention(person) for person in people_queue]
+ 
 
-# Send a single reminder message mentioning all people
-send_reminder(mentions)
+# Send reminders to each person in the queue
+for person in people_queue:
+    send_reminder(person)
